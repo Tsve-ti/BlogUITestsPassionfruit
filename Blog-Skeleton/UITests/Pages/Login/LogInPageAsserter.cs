5b12a36169d298a0d7475512ed4770f7e92160cd
Tsve-ti <moito_id@yahoo.com>
2017-05-12 19:12:14 +0300

    UI Tests First Try

---
+++ Blog-Skeleton/UITests/Pages/Login/LogInPageAsserter.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UITests.Pages.Login
+{
+    public static class LogInPageAsserter
+    {
+        public static void AssertLogInPageIsOpen(this LogInPage page, string text)
+        {
+            Assert.AreEqual(text, page.Header.Text);
+        }
+
+        public static void AssertValidLogIn1(this LogInPage page, string text)
+        {
+            Assert.IsTrue(page.Hello.Displayed);
+            StringAssert.Contains(text, page.Hello.Text);
+        }
+
+        public static void AssertValidLogIn2(this LogInPage page, string text)
+        {
+            Assert.IsTrue(page.LogOff.Displayed);
+            StringAssert.Contains(text, page.LogOff.Text);
+        }
+
+        public static void AssertNoEmailAddressDetected(this LogInPage page, string text)
+        {
+            Assert.IsTrue(page.NoEmailAddressOrInvalidEmailAddressFormat.Displayed);
+            StringAssert.Contains(text, page.NoEmailAddressOrInvalidEmailAddressFormat.Text);
+        }
+
+        public static void AssertInvalidEmailAddressFormat(this LogInPage page, string text)
+        {
+            Assert.IsTrue(page.NoEmailAddressOrInvalidEmailAddressFormat.Displayed);
+            StringAssert.Contains(text, page.NoEmailAddressOrInvalidEmailAddressFormat.Text);
+        }
+
+        public static void AssertInvalidLogInAttemptEmailAndOrPassword(this LogInPage page, string text)
+        {
+            Assert.IsTrue(page.InvalidLogInAttempt.Displayed);
+            StringAssert.Contains(text, page.InvalidLogInAttempt.Text);
+        }
+
+        public static void AssertNoPassword(this LogInPage page, string text)
+        {
+            Assert.IsTrue(page.PasswordFieldRequired.Displayed);
+            StringAssert.Contains(text, page.PasswordFieldRequired.Text);
+        }
+
+    }
+}
---
+++ Blog-Skeleton/UITests/TestClass.cs
@@ -0,0 +1,154 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+using NUnit.Framework.Interfaces;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using UITests.Pages.Login;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.Drawing;
+using UITests.Models;
+using static UITests.Pages.BasePage;
+using OpenQA.Selenium.Firefox;
+
+namespace UITests
+{
+    [TestFixture]
+    public class TestClass
+    {
+
+        public IWebDriver driver;
+
+        [SetUp]
+        public void Init()
+        {
+            this.driver = BrowserHost.Instance.Application.Browser;
+            this.driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+
+
+            if (TestContext.CurrentContext.Result.Outcome.Status == TestStatus.Failed)
+            {
+                //var regPage = new RegistrationPage(driver);
+                //regPage.ZoomOut();
+
+                string filename = Environment.CurrentDirectory + ConfigurationManager.AppSettings["Logs"] + TestContext.CurrentContext.Test.Name + ".jpg";
+
+                var screenshot = ((ITakesScreenshot)this.driver).GetScreenshot();
+
+                screenshot.SaveAsFile(filename, ScreenshotImageFormat.Jpeg);
+
+            }
+   
+        }
+
+        [Test, Property("Priority", 1)]
+        [Author("Anonymous")]
+        public void ValidLogIn()
+        {
+
+            //IWebDriver driver = BrowserHost.Instance.Application.Browser;
+            //WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+
+            var loginPage = new LogInPage(this.driver);
+            var logIn = AccessExcelData.GetTestData("ValidLogin");
+            loginPage.NavigatetoBlogLogIn();
+            loginPage.FillLogInForm(logIn);
+            loginPage.AssertValidLogIn1(("Hello"));
+            loginPage.AssertValidLogIn2("Log off");
+        }
+
+        [Test, Property("Priority", 1)]
+        [Author("Anonymous")]
+        public void TryToLogInNoEmailAddress()
+        {
+            var loginPage = new LogInPage(this.driver);
+            var logIn = AccessExcelData.GetTestData("No Email Address");
+            loginPage.NavigatetoBlogLogIn();
+            loginPage.FillLogInForm(logIn);
+            loginPage.AssertNoEmailAddressDetected("The Email field is required");
+        }
+
+        [Test, Property("Priority", 1)]
+        [Author("Anonymous")]
+        public void TryToLogInIncorrectEmailAddress()
+        {
+            var loginPage = new LogInPage(this.driver);
+            var logIn = AccessExcelData.GetTestData("Incorrect Email Address");
+            loginPage.NavigatetoBlogLogIn();
+            loginPage.FillLogInForm(logIn);
+            loginPage.AssertInvalidLogInAttemptEmailAndOrPassword("Invalid login attempt");
+        }
+
+        [Test, Property("Priority", 1)]
+        [Author("Anonymous")]
+        public void TryToLogInInvalidEmailAddressFormat()
+        {
+            var loginPage = new LogInPage(this.driver);
+            var logIn = AccessExcelData.GetTestData("Invalid Email Address Format");
+            loginPage.NavigatetoBlogLogIn();
+            loginPage.FillLogInForm(logIn);
+            loginPage.AssertInvalidEmailAddressFormat("The Email field is not a valid e-mail address");
+        }
+
+        [Test, Property("Priority", 1)]
+        [Author("Anonymous")]
+        public void TryToLogInIncorrectPassword()
+        {
+            var loginPage = new LogInPage(this.driver);
+            var logIn = AccessExcelData.GetTestData("Incorrect Password");
+            loginPage.NavigatetoBlogLogIn();
+            loginPage.FillLogInForm(logIn);
+            loginPage.AssertInvalidLogInAttemptEmailAndOrPassword("Invalid login attempt");
+        }
+
+        [Test, Property("Priority", 1)]
+        [Author("Anonymous")]
+        public void TryToLogInNoPassword()
+        {
+            var loginPage = new LogInPage(this.driver);
+            var logIn = AccessExcelData.GetTestData("No Password");
+            loginPage.NavigatetoBlogLogIn();
+            loginPage.FillLogInForm(logIn);
+            loginPage.AssertNoPassword("The Password field is required");
+        }
+
+        [Test, Property("Priority", 1)]
+        [Author("Anonymous")]
+        public void TryToLogInNoEmailAdressAndNoPassword()
+        {
+            var loginPage = new LogInPage(this.driver);
+            var logIn = AccessExcelData.GetTestData("No email address and no password");
+            loginPage.NavigatetoBlogLogIn();
+            loginPage.FillLogInForm(logIn);
+            loginPage.AssertNoEmailAddressDetected("The Email field is required");
+            loginPage.AssertNoPassword("The Password field is required");
+        }
+
+        [Test, Property("Priority", 1)]
+        [Author("Anonymous")]
+        public void TryToLogInInvalidEmailAdressFormatAndNoPassword()
+        {
+            var loginPage = new LogInPage(this.driver);
+            var logIn = AccessExcelData.GetTestData("Invalid email address format and no password");
+            loginPage.NavigatetoBlogLogIn();
+            loginPage.FillLogInForm(logIn);
+            loginPage.AssertInvalidEmailAddressFormat("The Email field is not a valid e-mail address");
+            loginPage.AssertNoPassword("The Password field is required");
+        }
+
+    }
+}
